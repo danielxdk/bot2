e52833485595ae19cd6fb9b5e5ab39a8defacf20
scifidemon <46400922+scifidemon@users.noreply.github.com>
2020-04-03 00:35:08 +0545

    Fixed some cc bugs

--- userbot/plugins/cc_check.py
+++ userbot/plugins/cc_check.py
@@ -15,18 +15,18 @@ async def _(event):
     cclist = event.text.split(" ")
     n = len(cclist)
     await event.edit("Checking...")
+    final_result=""
     for i in range(1,n):
         cc = cclist[i]
         URL = f"https://api.validcc.pro/?key=r21NZ5AODUqL101hPNeN&cc={cc}"
         r = get(URL).json()
         code = f"Code: {r['code']}\n"
         status = f"Status: {r['status']}\n"
-        card = f"Card: "+ "`{r['card']}`\n"
+        card = "Card: "+ f"`{r['card']}`\n"
         rem_req = f"Remaining requests: {r['requests_remaining']}\n"
         result = code+status+card+rem_req
-        final_result=""
         final_result = final_result+result
-        await event.edit(final_result)
+        await event.edit(final_result+"Checking more...")
     await event.edit(final_result+"DONE CHECKING!!!")
         
 
